436794ff7d52771c51b819922c2cda0e617891d5
Rochdi BAZINE <wisi122@domwisitn.local>
2018-09-20 16:48:19 +0100

    Fix folders listing in the config file, adding backup prefix in config file

--- backup_to_ftp.py
+++ backup_to_ftp.py
@@ -54,111 +54,128 @@ def makeArchive(fileList, archive):
     except: return False
  
 def dirEntries(dir_name, subdir, *args):
-    '''Return a list of file names found in directory 'dir_name'
-    If 'subdir' is True, recursively access subdirectories under 'dir_name'.
-    Additional arguments, if any, are file extensions to match filenames. Matched
-        file names are added to the list.
-    If there are no additional arguments, all files found in the directory are
-        added to the list.
-    Example usage: fileList = dirEntries(r'H:\TEMP', False, 'txt', 'py')
-        Only files with 'txt' and 'py' extensions will be added to the list.
-    Example usage: fileList = dirEntries(r'H:\TEMP', True)
-        All files and all the files in subdirectories under H:\TEMP will be added
-        to the list.
-    '''
-    fileList = []
-    for file in os.listdir(dir_name):
-        dirfile = os.path.join(dir_name, file)
-        if os.path.isfile(dirfile):
-            if not args:
-                fileList.append(dirfile)
-            else:
-                if os.path.splitext(dirfile)[1][1:] in args:
-                    fileList.append(dirfile)
-        # recursively access file names in subdirectories
-        elif os.path.isdir(dirfile) and subdir:
-            fileList.extend(dirEntries(dirfile, subdir, *args))
-    return fileList
+	'''Return a list of file names found in directory 'dir_name'
+	If 'subdir' is True, recursively access subdirectories under 'dir_name'.
+	Additional arguments, if any, are file extensions to match filenames. Matched
+		file names are added to the list.
+	If there are no additional arguments, all files found in the directory are
+		added to the list.
+	Example usage: fileList = dirEntries(r'H:\TEMP', False, 'txt', 'py')
+		Only files with 'txt' and 'py' extensions will be added to the list.
+	Example usage: fileList = dirEntries(r'H:\TEMP', True)
+		All files and all the files in subdirectories under H:\TEMP will be added
+		to the list.
+	'''
+	fileList = []
+	try :
+		for file in os.listdir(dir_name):
+			dirfile = os.path.join(dir_name, file)
+			if os.path.isfile(dirfile):
+				if not args:
+					fileList.append(dirfile)
+				else:
+					if os.path.splitext(dirfile)[1][1:] in args:
+						fileList.append(dirfile)
+			# recursively access file names in subdirectories
+			elif os.path.isdir(dirfile) and subdir:
+				fileList.extend(dirEntries(dirfile, subdir, *args))
+	except :
+		print " > Directory not found " + dir_name 
+				
+	return fileList
 	
 if __name__ == '__main__':
 	print "ZIP to FTP Backup Tool"
-	print "v0.0.1 - Johann du Toit"
-	print "----------"
+	print "v0.1.0 - Credits to Johann du Toit"
+	print "-----------------------------------"
+	backup_prefix = config.get("default", 'backup_prefix')
 
 	filelistings = [] # All Files that will be added to the Archive
 
-	print "\n====="
 	# Add Files From Locations
-	for folder in config.options("folders"):
-			print "Scanning " + str(folder.strip())
-			for fi in dirEntries(folder.strip(' \t\n\r'), True):
-					if "WHAT IS TRANSFER.txt" not in fi:
-							filelistings.append(fi)
-	print "=====\n"
+	for key, folder in  config.items( "folders" ):
+	#for folder in  config.get("folders", "list"):
+		print "Scanning " + str(folder.strip())
+		directory_entries=dirEntries(folder.strip(' \t\n\r'), True)
+		if directory_entries:
+			print " > " + str(len(directory_entries)) + " files found"
+		else:
+			print " > Nothing is out there" 
+		
+		for fi in directory_entries:
+			if "WHAT IS TRANSFER.txt" not in fi:
+				filelistings.append(fi)
 
-	now = datetime.datetime.now()
-	zipname = r'shaka.' + now.strftime("%d.%m.%Y") + '.zip'
 
-	print "\n====="
-	print "Add Backup to a ZIP File"
-	makeArchive(filelistings, zipname)
-	print "Backup was Zipped"
-	print "=====\n"
+	if not filelistings:
+		print "Nothingto backup..."
+	else :
+		now = datetime.datetime.now()
+		zipname = r'' + backup_prefix + now.strftime("%Y.%m.%d") + '.zip'
+		
+		print ""
+		print "Add Backup to a ZIP File"
+		print "Total files to be zipped " + str(len(filelistings))  
 
-	print "\n====="
-	print "Connecting to FTP Server..."
+		makeArchive(filelistings, zipname)
+		print "Backup was Zipped to " + zipname
 
-	ftp = ftplib.FTP(config.get("ftp", 'host'))
-	
-	print "Logging in..."
-	
-	ftp.login(config.get("ftp", 'username'), config.get("ftp", 'password'))
+		print ""
+		print "Connecting to FTP Server..."
 
-	print "Getting Directory Listing..."
-	
-	files = ftp.nlst()
-	
-	print "Found " + str(len(files)) + " files"
-	
-	print "Removing Stale Backups older than " + str(config.get("ftp", 'keep_for_months').strip()) + " Month(s)"
-	
-	count_deleted = 0
-	months_from_now = int((datetime.date.today() + datetime.timedelta( ( int(config.get("ftp", 'keep_for_months').strip()) *365) / 12 )).strftime("%m"))
-	
-	for file in files:
-		if "shaka." in file:
-			file_parts = file.split('.')
-			if len(file_parts) == 5:
-				year = int(file_parts[3])
-				month = int(file_parts[2])
-				date = int(file_parts[1])
-				
-				date_month_diff = diff_month(datetime.datetime( int(now.strftime("%Y")), int(now.strftime("%m")), int(now.strftime("%d")) ), datetime.datetime(year,month,date))
-				
-				if date_month_diff >= int(config.get("ftp", 'keep_for_months').strip()):
-					count_deleted = count_deleted + 1
+		ftp = ftplib.FTP(config.get("ftp", 'host'))
+						
+		print "Logging in..."
+		ftp.login(config.get("ftp", 'username'), config.get("ftp", 'password'))
+
+		print ""				
+		print "Getting Directory Listing..."
+		files = ftp.nlst()
+		print "Found " + str(len(files)) + " files"
+		print "Removing Stale Backups older than " + str(config.get("ftp", 'keep_for_months').strip()) + " Month(s)"
+		
+		count_deleted = 0
+		months_from_now = int((datetime.date.today() + datetime.timedelta( ( int(config.get("ftp", 'keep_for_months').strip()) *365) / 12 )).strftime("%m"))
+		
+		for file in files:
+			if backup_prefix in file:
+				file_parts = file.split('.')
+				if len(file_parts) == 5:
+					year = int(file_parts[3])
+					month = int(file_parts[2])
+					date = int(file_parts[1])
 					
-					try:
-						print "Removing Backup " + str(file) + " it's " + str(date_month_diff) + " Month(s) Old"
-						ftp.delete(file)
-					except:
-						pass
+					date_month_diff = diff_month(datetime.datetime( int(now.strftime("%Y")), int(now.strftime("%m")), int(now.strftime("%d")) ), datetime.datetime(year,month,date))
+					
+					if date_month_diff >= int(config.get("ftp", 'keep_for_months').strip()):
+						count_deleted = count_deleted + 1
+						
+						try:
+							print "Removing Backup " + str(file) + " it's " + str(date_month_diff) + " Month(s) Old"
+							ftp.delete(file)
+						except:
+							pass
+		
+		print "Removed " + str(count_deleted) + " Backups"
+		
+		print ""
+		print "Uploading Backup Zip..."
+		
+		try:
+			ftp.storbinary('STOR ' + zipname, open(zipname, 'rb'))
+		except:
+			pass
+				
+		print "Done."
+		print "Closing Connection..."
+		
+		ftp.close()
+		
+		print ""
+		print "Deleting local Backup ZIP"
+		os.remove(zipname)
+		
 	
-	print "Removed " + str(count_deleted) + " Backups"
-	
-	print "Uploading Backup Zip..."
-	
-	try:
-		ftp.storbinary('STOR ' + zipname, open(zipname, 'rb'))
-	except:
-		pass
-	
-	print "Closing Connection..."
-	
-	ftp.close()
-	
-	print "Deleting Backup ZIP"
-	os.remove(zipname)
-	
-	print "=====\n"
+		
+     
  
